4f9ec6150a7aabae138db46216a2da3481daa980
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2022-12-22 10:09:28 -0600

    clean/organize_01

--- assets/script.js
+++ assets/script.js
@@ -98,25 +98,3 @@ function generatePassword(){
 
 
 
-
-    //  var to save length
-    //  validate user num 
-  // confirm user upper, lower, num, special characters ( 4 confirm prompts)
-    
-    // 4 vars to save T/F to included characters
-    // Validate that atleast one cat was chosen
-  // Vars that include all user options of each category. Array of string for each cat.
-  // var upper = [" A", "B"]
-  // If user says yes to include that cat choose random(Math.random) elements from the array
-    // How to decide how many elements to choose from each array
-  //
-  // Join chosen elements into a string
-  // return passwordString; 
-
-
-// }
-
-
-
-
-
