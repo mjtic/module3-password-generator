e75428848377e8928bbc47eb731764c3382ae4ed
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2022-12-21 19:21:23 -0600

    having git issues, tried rebased but didn't work, deleted brances from both local and github and starting again.. i love it

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+// global var
+// array for: lowerCase, upperCase, number, and specialCharacters
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var specialCharacters = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+"-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
+
+var possibleCharacters = [];
+
+var userSelLow = false;
+var userSelUpp = false;
+var userSelNum = false;
+var userSelSpC = false;
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -14,6 +32,41 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
+
+ // optional message, and to wait until the user dismisses the dialog.
+ window.alert('User have four criteria options and must select at least one of the criterias'); 
+
+// Prompt user to choose num btwn 8 and 128 (display a dialog with an optional message prompting the user to input some text, and to wait until the user either submits the text or cancels the dialog.)
+  // make  sure user enters a number
+  confirmLength = prompt('How long of characters should your password be? Choose between 8 - 128')
+  parseInt(confirmLength) > 8 && parseInt (confirmLength) <128;
+
+  // make sure user entered a number between 8 -128
+  
+
+  // does user want lowercase letters
+  userSelLow = confirm ('Should your password contain lowercase?')
+  if(userSelLow === 'true') {
+  possibleCharacters.push(...lowerCase)
+  }
+  // uppercase
+  userSelUpp = confirm ('Should your passoword contain uppercase?')
+
+  //special
+  userSelSpC = confirm ('Should your password contain Special Chracters?')
+
+  // numbers
+  userSelNum = confirm ('Should your password contain numbers?')
+
+  //based on user answers add characters to possible char array
+
+
+
+
+
+
+
+
   // must return a string value that is the password.
   // Prompt user to choose num btwn 8 and 128
     //  var to save length
